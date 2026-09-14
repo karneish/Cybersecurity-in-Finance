7e415fc0e924eef16a1f460fd5a41b988ee0de10
karneish <karneish.sk@gmail.com>
2026-09-14 15:53:12 +0530

    add services/common/cybercommon/database.py

---
+++ services/common/cybercommon/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from cybercommon.config import settings
+
+engine = create_engine(
+    settings.database_url,
+    pool_pre_ping=settings.db_pool_pre_ping,
+    pool_size=settings.db_pool_size,
+    max_overflow=settings.db_max_overflow,
+    pool_recycle=settings.db_pool_recycle,
+    pool_timeout=settings.db_pool_timeout,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
